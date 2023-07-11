cf2023a1cdd77dd92a19ba27105d9e63b12d775a
erykerlich <eryk2023@hotmail.com>
2023-07-11 13:49:41 -0300

    Login, id, senha...

---
+++ src/main/java/com/eryk/myapi/domain/Usuario.java
@@ -0,0 +1,78 @@
+package com.eryk.myapi.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Usuario implements Serializable {
+    //pega uma sequencia armazenada em rede
+    private static final long serialVesionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    //Não pode ser vazio
+    @NotEmpty(message = "Compo NOME é obrigatório")
+    @Length(min = 3, max = 100, message = "O NOME deve ter entr 3 e 100 caracteres")
+    private String nome;
+    @NotEmpty(message = "Compo LOGIN é obrigatório")
+    @Length(min = 3, max = 100, message = "O LOGIN deve ter entr 3 e 100 caracteres")
+    private String login;
+    @NotEmpty(message = "Compo SENHA é obrigatório")
+    @Length(min = 3, max = 100, message = "A SENHA deve ter entr 3 e 100 caracteres")
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id, String nome, String login, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+        this.login = login;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
